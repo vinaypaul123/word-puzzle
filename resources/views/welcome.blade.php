7453072a84be21d47772854778bd7e6025e01d60
vinaypaul123 <vinaypaulkarimathy@gmail.com>
2025-05-21 23:36:58 +0530

    puzzle game

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <h1>Welcome to Word Puzzle Game</h1>
-    <button class="btn btn-primary mb-2"><a style="color:#fff;text-decoration:none" href="{{ route('puzzle.name') }}">Start Puzzle</a></button><br>
-     <button class="btn btn-primary"><a style="color:#fff;text-decoration:none" href="{{ route('leaderboard') }}">Leader Board </button></a>
+    <a style="color:#fff;text-decoration:none" href="{{ route('puzzle.name') }}"><button class="btn btn-primary mb-2">Start Puzzle</button></a><br>
+     <a style="color:#fff;text-decoration:none" href="{{ route('leaderboard') }}"><button class="btn btn-primary">Leader Board </button></a>
 @extends('layout')
 
 @section('content')
